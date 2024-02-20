e6177e6e9e2ec966d81c5ba0d97bf7dda1a48c39
juhid30 <55.juhideore@gmail.com>
2024-02-20 23:49:02 +0530

    new

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-var today = new Date();
-var date =
-  today.getDate() +
-  "/" +
-  (today.getMonth() + 1) +
-  "/" +
-  today.getFullYear() +
-  "   ";
-var daylist = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-var day = today.getDay();
+function updateTime() {
+  var today = new Date();
+  var month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ][today.getMonth()];
+  var date =
+    String(today.getDate()).padStart(2, "0") +
+    " " +
+    month +
+    " " +
+    today.getFullYear();
+  var daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var day = today.getDay();
 
-var time =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-document.getElementById("todaydate").innerHTML =
-  "Today,  " + daylist[day] + "  " + date + "<br>" + time;
+  var time =
+    String(today.getHours()).padStart(2, "0") +
+    ":" +
+    String(today.getMinutes()).padStart(2, "0") +
+    ":" +
+    String(today.getSeconds()).padStart(2, "0");
+  document.getElementById("todaydate").innerHTML =
+    "Today,  " + daylist[day] + "  " + date + "<br>" + time;
+}
+updateTime();
+setInterval(updateTime, 1000);
 
 // Add modal
 var modalToAdd = document.getElementById("createModal");
@@ -114,8 +127,16 @@ var close_EditModalBtn = document.getElementsByClassName("editmodal-cancel")[0];
 function editTodo(i) {
   modalToEdit.style.display = "block";
   console.log(todoArray[i]);
-  /*  var editInputText = document.querySelector("#editmodal-inp-text");
-  editInputText.value = todoArray[i]; */
+  var editModal_InputText = document.querySelector("#editmodal-inp-text");
+  editModal_InputText.value = todoArray[i].todos;
+
+  var editModal_Time = document.querySelector("#editmodal-time-input");
+  editModal_Time.value = todoArray[i].todotime;
+  editModal_InputText.focus();
+  editModal_InputText.ariaSelected();
+
+  var saveBtn_EditModal = document.getElementById("editmodal-savebtn");
+  saveBtn_EditModal.addEventListener("click", displayEditedTodo(i));
 }
 close_EditModalBtn.onclick = function () {
   modalToEdit.style.display = "none";
@@ -125,26 +146,22 @@ window.onclick = function (event) {
     modalToEdit.style.display = "none";
   }
 };
-var saveBtn_EditModal = document.getElementById("editmodal-savebtn");
-
+/* var saveBtn_EditModal = document.getElementById("editmodal-savebtn");
 saveBtn_EditModal.onclick = function () {
   modalToEdit.style.display = "none";
   console.log("abcd");
   var editInputBox = document.querySelector("#editmodal-inp-text");
-  /*   editInputBox.innerHTML = `${todoArray[i]}`; */
+  
   displayEditedTodo(editInputBox.value, todoArray.indexOf(editInputBox.value));
-};
-function displayEditedTodo(editedInput, i) {
+}; */
+function displayEditedTodo(i) {
+  modalToEdit.style.display = "none";
+  console.log("abcd");
   var editedInputBox = document.querySelector("#editmodal-inp-text");
 
-  if (i > -1) {
-    //todo exists in the array
-    todoArray.splice(i, 1);
+  todoArray.splice(i, 1);
 
-    todoArray[i] = editedInputBox.value;
-  }
-
-  todoArray[i] = editedInputBox.value;
+  todoArray[i].todos = editedInputBox.value;
 
   updateTodosInLocalStorage();
   addTodosInHTML();
